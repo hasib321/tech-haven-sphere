cf2ac8aef50fa9a7ca81ed99aabedf81fa1f1ea0
AbrRahman <abrahman.est@gamil.com>
2024-10-01 00:01:25 +0600

    create order interface and order model

---
+++ src/app/modules/order.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+import { Order } from "./order/order.interface";
+
+const OrderSchema=new Schema<Order>({
+    email:{
+        type:String,
+        lowercase:true,
+        trim:true
+    },
+    productId:{
+        type:String,
+        trim:true,
+    },
+    price:{
+        type:Number,
+    },
+    quantity:{
+        type:Number
+    }
+})
+
+const OrderModel= mongoose.model("order",OrderSchema);
+export default OrderModel;
